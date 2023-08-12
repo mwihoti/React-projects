9733e019ec93a97164458e1a4fe97925df6c62fe
mwihoti <danielmwihoti@gmail.com>
2023-08-12 16:25:00 +0300

    My react projects

---
+++ multimedia-app/src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/React authentication using JSON Server REST API _ Registration screen _React JS Full Course.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Mp3Juice-dot-Pet-Roddy-Ricch-The-Box-Wireless-Festival-2022-.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/pyhon.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/2.png"
+    },
+    {
+      id: 5,
+      name: 'Image File',
+      type: 'document',
+      path: "/file-server/1.png"
+    },
+    {
+      id: 6,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/4.png"
+    }
+  ]
